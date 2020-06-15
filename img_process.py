71afea1909879c123c75dc24949f691ac0fa7838
GuyARoss <guyallenross@gmail.com>
2020-06-14 23:28:43 -0400

    t1

--- img_process.py
+++ img_process.py
@@ -1,63 +1,33 @@
 import numpy as np
 import cv2
 
-# @@ des wrong
-YELLOW = (255, 215, 51)
-RED = (255, 51, 51)
-ORANGE = (255, 87, 51)
-GREEN = (51, 255, 97)
-
-
-def get_color_mapping(color_str):
-    return {
-        'yellow': YELLOW,
-        'red': RED,
-        'orange': ORANGE,
-        'green': GREEN,
-    }[color_str]
-
 
 def write(txt):
-    mp = map(lambda x: x[0], txt)
-    w = " ".join(mp)
+    mp = max(list(map(lambda x: len(x), txt)))
 
-    img = np.zeros((300, len(w * 30), 3), np.uint8)
+    scaley = 68
+    h = 100 + len(txt) * scaley
+    w = mp * 45
+
+    img = np.zeros((h, w, 3), np.uint8)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     fontScale = 2
     lineType = 2
 
-    offset = 60
-    for idx in range(len(txt)):
-        i = txt[idx]
-        color = get_color_mapping(i[1])
-        f = len(i[0]) * 30
-
-        cv2.putText(img, i[0],
-                    (len(i[0]) + 20 + offset + (idx * 15), 150),
+    for sidx in range(len(txt)):
+        st = txt[sidx]
+        off = sidx * scaley
+        cv2.putText(img, st,
+                    (60, off + 100),
                     font,
                     fontScale,
-                    color,
+                    (255, 255, 255),
                     lineType)
 
-        offset += f
-
-    cv2.imshow("img", img)
     cv2.imwrite("out.jpg", img)
 
 
 if __name__ == "__main__":
-    write([
-        ('this', 'green'),
-        ('is', 'yellow'),
-        ('a', 'orange'),
-        ('test', 'red'),
-        ('to', 'orange'),
-        ('see', 'yellow'),
-        ('if', 'yellow'),
-        ('the', 'orange'),
-        ('colors', 'red'),
-        ('are', 'orange'),
-        ('working', 'yellow'),
-    ])
+    write(['turn', 'on the computer', 'and toaster'])
